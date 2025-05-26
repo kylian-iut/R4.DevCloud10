733d2a5dcaa8751c8d67d4ff05f14ac01bab67d1
kylian-iut <kadam.iut@gmail.com>
2025-05-26 09:44:44 +0200

    create commentairelistapiview

--- project/monprojet/monapi/urls.py
+++ project/monprojet/monapi/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
+from django.conf.urls import url
+from django.urls import path, include
+from .views import CommentaireListApiView
 
 urlpatterns = [
+    path('api/', CommentaireListApiView.as_view()),
     path('', views.index, name='index'),  # Exemple de route
 ]
