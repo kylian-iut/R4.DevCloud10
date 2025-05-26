1362d33ae8e0539c605d3825b365ea72a74f630b
kylian-iut <kadam.iut@gmail.com>
2025-05-26 10:30:59 +0200

    correction

--- project/monprojet/monapi/models.py
+++ project/monprojet/monapi/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Commentaire(models.Model):
     titre = models.CharField(max_length=200)  # titre du commentaire
     commentaire = models.TextField()           # texte du commentaire
     date_publication = models.DateField(auto_now=True)  # date de publication auto à la mise à jour
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='commentaires')
 
     def __str__(self):
         return f"{self.titre} ({self.date_publication})"
--- project/monprojet/monapi/views.py
+++ project/monprojet/monapi/views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Commentaire
+from .serializer import CommentaireSerializer
 
 def index(request):
     return JsonResponse({"message": "Bienvenue sur mon API !"})
+
+class CommentaireListApiView(APIView):
+    def get(self, request, *args, **kwargs):
+        commentaires = Commentaire.objects.filter(user = request.user.id)
+        serializer = CommentaireSerializer(commentaires, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request, *args, **kwargs):
+        data = {
+            'titre': request.data.get('titre'),
+            'commentaire': request.data.get('commentaire'),
+        }
+        serializer = CommentaireSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 # Create your views here.
